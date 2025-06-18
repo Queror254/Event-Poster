60e273597ecc8fd99e93a2f268376eac7c8d078c
Queror254 <mwendavictorm@gmail.com>
2025-06-18 16:44:53 +0300

    change main-bg color

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   // }, []);
 
   return (
-    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 flex items-center justify-center p-2 sm:p-4 md:p-6 selection:bg-yellow-400 selection:text-neutral-900 font-sans">
+    <div className="min-h-screen bg-neutral-800 dark:bg-neutral-950 flex items-center justify-center p-2 sm:p-4 md:p-6 selection:bg-yellow-400 selection:text-neutral-900 font-sans">
       <Poster />
     </div>
   );
